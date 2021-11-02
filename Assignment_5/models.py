a0c92f8aee01a8a021678e35a5495ff5ede29e75
Craig Leavitt <craig_leavitt@hotmail.com>
2021-11-01 20:08:43 -0600

    Trying GRU encoder / decoder

--- Assignment_5/models.py
+++ Assignment_5/models.py
@@ -8,6 +8,9 @@ from data import *
 from lf_evaluator import *
 import numpy as np
 from typing import List
+from torch import optim
+
+device = torch.device("cpu")
 
 def add_models_args(parser):
     """
@@ -66,22 +69,13 @@ class Seq2SeqSemanticParser(nn.Module):
         super(Seq2SeqSemanticParser, self).__init__()
         self.input_indexer = input_indexer
         self.output_indexer = output_indexer
-        self.output_size = len(output_indexer)
-        
-        self.input_emb = EmbeddingLayer(emb_dim, len(input_indexer), embedding_dropout)
-        self.encoder = RNNEncoder(emb_dim, hidden_size, bidirect)
-        self.decoder = RNNDecoder(emb_dim, hidden_size, self.output_size)
+        output_size = len(output_indexer)
+
+        self.encoder = RNNEncoder(len(input_indexer), hidden_size).to(device)
+        self.decoder = AttnDecoderRNN(hidden_size, output_size, output_size-1).to(device)
         self.hidden_size = hidden_size
         self.embedding_dropout = embedding_dropout
         self.bidirect = bidirect
-
-
-        self.reduce_h_W = nn.Linear(hidden_size * 2, hidden_size, bias=True)
-        self.reduce_c_W = nn.Linear(hidden_size * 2, hidden_size, bias=True)
-        self.rnn = nn.LSTM(emb_dim, hidden_size, num_layers=1, batch_first=True,
-                           dropout=0., bidirectional=self.bidirect)
-        self.out = nn.Linear(hidden_size, self.output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
         
 
     def forward(self, x_tensor, inp_lens_tensor, y_tensor, out_lens_tensor):
@@ -97,150 +91,44 @@ class Seq2SeqSemanticParser(nn.Module):
     def decode(self, test_data: List[Example]) -> List[List[Derivation]]:
         raise Exception("implement me!")
 
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size)
-
-    def encode_input(self, x_tensor, inp_lens_tensor):
-        """
-        Runs the encoder (input embedding layer and encoder as two separate modules) on a tensor of inputs x_tensor with
-        inp_lens_tensor lengths.
-        YOU DO NOT NEED TO USE THIS FUNCTION. It's merely meant to illustrate the usage of EmbeddingLayer and RNNEncoder
-        as they're given to you, as well as show what kinds of inputs/outputs you need from your encoding phase.
-        :param x_tensor: [batch size, sent len] tensor of input token indices
-        :param inp_lens_tensor: [batch size] vector containing the length of each sentence in the batch
-        :param model_input_emb: EmbeddingLayer
-        :param model_enc: RNNEncoder
-        :return: the encoder outputs (per word), the encoder context mask (matrix of 1s and 0s reflecting which words
-        are real and which ones are pad tokens), and the encoder final states (h and c tuple). ONLY THE ENCODER FINAL
-        STATES are needed for the basic seq2seq model. enc_output_each_word is needed for attention, and
-        enc_context_mask is needed to batch attention.
-
-        E.g., calling this with x_tensor (0 is pad token):
-        [[12, 25, 0],
-        [1, 2, 3],
-        [2, 0, 0]]
-        inp_lens = [2, 3, 1]
-        will return outputs with the following shape:
-        enc_output_each_word = 3 x 3 x dim, enc_context_mask = [[1, 1, 0], [1, 1, 1], [1, 0, 0]],
-        enc_final_states = 3 x dim
-        """
-        input_emb = self.input_emb.forward(x_tensor)
-        (enc_output_each_word, enc_context_mask, enc_final_states) = self.encoder.forward(input_emb, inp_lens_tensor)
-        enc_final_states_reshaped = (enc_final_states[0].unsqueeze(0), enc_final_states[1].unsqueeze(0))
-        return (enc_output_each_word, enc_context_mask, enc_final_states_reshaped)
-
-
-class EmbeddingLayer(nn.Module):
-    """
-    Embedding layer that has a lookup table of symbols that is [full_dict_size x input_dim]. Includes dropout.
-    Works for both non-batched and batched inputs
-    """
-    def __init__(self, input_dim: int, full_dict_size: int, embedding_dropout_rate: float):
-        """
-        :param input_dim: dimensionality of the word vectors
-        :param full_dict_size: number of words in the vocabulary
-        :param embedding_dropout_rate: dropout rate to apply
-        """
-        super(EmbeddingLayer, self).__init__()
-        self.dropout = nn.Dropout(embedding_dropout_rate)
-        self.word_embedding = nn.Embedding(full_dict_size, input_dim)
-
-    def forward(self, input):
-        """
-        :param input: either a non-batched input [sent len x voc size] or a batched input
-        [batch size x sent len x voc size]
-        :return: embedded form of the input words (last coordinate replaced by input_dim)
-        """
-        embedded_words = self.word_embedding(input)
-        final_embeddings = self.dropout(embedded_words)
-        return final_embeddings
-
 
 class RNNEncoder(nn.Module):
     """
     One-layer RNN encoder for batched inputs -- handles multiple sentences at once. To use in non-batched mode, call it
     with a leading dimension of 1 (i.e., use batch size 1)
     """
-    def __init__(self, input_emb_dim: int, hidden_size: int, bidirect: bool):
+    def __init__(self, input_size, hidden_size):
         """
         :param input_emb_dim: size of word embeddings output by embedding layer
         :param hidden_size: hidden size for the LSTM
         :param bidirect: True if bidirectional, false otherwise
         """
         super(RNNEncoder, self).__init__()
-        self.bidirect = bidirect
         self.hidden_size = hidden_size
-        self.reduce_h_W = nn.Linear(hidden_size * 2, hidden_size, bias=True)
-        self.reduce_c_W = nn.Linear(hidden_size * 2, hidden_size, bias=True)
-        self.rnn = nn.LSTM(input_emb_dim, hidden_size, num_layers=1, batch_first=True,
-                               dropout=0., bidirectional=True)
 
-    def get_output_size(self):
-        return self.hidden_size * 2 if self.bidirect else self.hidden_size
-
-    def sent_lens_to_mask(self, lens, max_length):
-        return torch.from_numpy(np.asarray([[1 if j < lens.data[i].item() else 0 for j in range(0, max_length)] for i in range(0, lens.shape[0])]))
-
-    def forward(self, embedded_words, input_lens):
-        """
-        Runs the forward pass of the LSTM
-        :param embedded_words: [batch size x sent len x input dim] tensor
-        :param input_lens: [batch size]-length vector containing the length of each input sentence
-        :return: output (each word's representation), context_mask (a mask of 0s and 1s
-        reflecting where the model's output should be considered), and h_t, a *tuple* containing
-        the final states h and c from the encoder for each sentence.
-        Note that output is only needed for attention, and context_mask is only used for batched attention.
-        """
-        # Takes the embedded sentences, "packs" them into an efficient Pytorch-internal representation
-        packed_embedding = nn.utils.rnn.pack_padded_sequence(embedded_words, input_lens, batch_first=True, enforce_sorted=False)
-        # Runs the RNN over each sequence. Returns output at each position as well as the last vectors of the RNN
-        # state for each sentence (first/last vectors for bidirectional)
-        output, hn = self.rnn(packed_embedding)
-        # Unpacks the Pytorch representation into normal tensors
-        output, sent_lens = nn.utils.rnn.pad_packed_sequence(output)
-        max_length = max(input_lens.data).item()
-        context_mask = self.sent_lens_to_mask(sent_lens, max_length)
-
-        if self.bidirect:
-            h, c = hn[0], hn[1]
-            # Grab the representations from forward and backward LSTMs
-            h_, c_ = torch.cat((h[0], h[1]), dim=1), torch.cat((c[0], c[1]), dim=1)
-            # Reduce them by multiplying by a weight matrix so that the hidden size sent to the decoder is the same
-            # as the hidden size in the encoder
-            new_h = self.reduce_h_W(h_)
-            new_c = self.reduce_c_W(c_)
-            h_t = (new_h, new_c)
-        else:
-            h, c = hn[0][0], hn[1][0]
-            h_t = (h, c)
-        return (output, context_mask, h_t)
-
-class RNNDecoder(nn.Module):
-    """
-    One-layer RNN encoder for batched inputs -- handles multiple sentences at once. To use in non-batched mode, call it
-    with a leading dimension of 1 (i.e., use batch size 1)
-    """
-    def __init__(self, input_emb_dim: int, hidden_size: int, output_size: int):
-        """
-        :param input_emb_dim: size of word embeddings output by embedding layer
-        :param hidden_size: hidden size for the LSTM
-        :param bidirect: True if bidirectional, false otherwise
-        """
-        super(RNNDecoder, self).__init__()
-        self.embedding = nn.Embedding(output_size, hidden_size)
-        self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.rnn = nn.LSTM(input_emb_dim, hidden_size, num_layers=1, batch_first=True,
-                           dropout=0., bidirectional=False)
-        self.out = nn.Linear(hidden_size, self.output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
-
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
 
     def forward(self, input, hidden):
-        """
-        Runs the forward pass of the LSTM
-        """
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
 
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+class RNNDecoder(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        super(RNNDecoder, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
         output = self.embedding(input).view(1, 1, -1)
         output = F.relu(output)
         output, hidden = self.gru(output, hidden)
@@ -248,11 +136,11 @@ class RNNDecoder(nn.Module):
         return output, hidden
 
     def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size)
+        return torch.zeros(1, 1, self.hidden_size, device=device)
 
 
 class AttnDecoderRNN(nn.Module):
-    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=20):
+    def __init__(self, hidden_size, output_size, max_length, dropout_p=0.1):
         super(AttnDecoderRNN, self).__init__()
         self.hidden_size = hidden_size
         self.output_size = output_size
@@ -318,6 +206,63 @@ def make_padded_output_tensor(exs, output_indexer, max_len):
     """
     return np.array([[ex.y_indexed[i] if i < len(ex.y_indexed) else output_indexer.index_of(PAD_SYMBOL) for i in range(0, max_len)] for ex in exs])
 
+def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length, tokens):
+    teacher_forcing_ratio = 0.5
+
+    encoder_hidden = encoder.initHidden()
+
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    input_length = input_tensor.size(0)
+    target_length = target_tensor.size(0)
+
+    encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+
+    loss = 0
+
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(
+            input_tensor[ei], encoder_hidden)
+        encoder_outputs[ei] = encoder_output[0, 0]
+
+    print(f'Encoder Outputs shape: {encoder_outputs.shape}')
+
+    decoder_input = torch.tensor([[tokens[0]]], device=device)
+
+    print(f'Decoder Input: {decoder_input}')
+
+    decoder_hidden = encoder_hidden
+
+    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+
+    if use_teacher_forcing:
+        # Teacher forcing: Feed the target as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            loss += criterion(decoder_output, target_tensor[di])
+            decoder_input = target_tensor[di]  # Teacher forcing
+
+    else:
+        # Without teacher forcing: use its own predictions as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            topv, topi = decoder_output.topk(1)
+            decoder_input = topi.squeeze().detach()  # detach from history as input
+
+            loss += criterion(decoder_output, target_tensor[di])
+            if decoder_input.item() == tokens[1]:
+                break
+
+    loss.backward()
+
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return loss.item() / target_length
+
 
 def train_model_encdec(train_data: List[Example], dev_data: List[Example], input_indexer, output_indexer, args) -> Seq2SeqSemanticParser:
     """
@@ -356,10 +301,46 @@ def train_model_encdec(train_data: List[Example], dev_data: List[Example], input
     NUM_LAYERS = 3
     HIDDEN_DIM = 96
     DROP_PROB = 0.2
-    TEACHER_FORCING_RATIO = 0.5
+    learning_rate = 0.01
+    tokens = []
+    epochs = 5000
+    print_every = 500
+    tokens.append(int(0))
+    tokens.append(int(1))
+    print(f'Tokens: {tokens}')
 
     s2smodel = Seq2SeqSemanticParser(input_indexer,output_indexer, EMBED_DIM, HIDDEN_DIM, DROP_PROB)
+    encoder = s2smodel.encoder
+    decoder = s2smodel.decoder
     print('Encoded Tensor Example ------------------------------------------')
-    encoded = s2smodel.encode_input(torch.unsqueeze(torch.Tensor(all_train_input_data[0]).type(torch.LongTensor),0), torch.Tensor([input_max_len]).type(torch.LongTensor))
-    print(encoded)
+    #input = s2smodel.encode_input(torch.Tensor(all_train_input_data).type(torch.LongTensor), torch.Tensor([input_max_len]).type(torch.LongTensor))
+    input = torch.Tensor(all_train_input_data).type(torch.LongTensor)
+    input = input.to(device)
+    output = torch.Tensor(all_train_output_data).type(torch.LongTensor)
+    output = output.to(device)
+
+    print(f'Input Shape: {input.shape}')
+
+    print(f'Output Shape: {output.shape}')
+
+    encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
+    criterion = nn.NLLLoss()
+
+    for iter in range(1, epochs + 1):
+
+        input_tensor = torch.unsqueeze(input[iter-  1], 1)
+        target_tensor = torch.unsqueeze(output[iter - 1], 1)
+        print(f'Input Tensor Shape: {input_tensor.shape}')
+        print(f'Input Tensor: {input_tensor}')
+        print(f'Target Tensor Shape: {target_tensor.shape}')
+        print(f'Target Tensor: {target_tensor}')
+        loss = train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, output_max_len, tokens)
+        print_loss_total += loss
+
+        if iter % print_every == 0:
+            print_loss_avg = print_loss_total / print_every
+            print_loss_total = 0
+            print(f'Epoch: {iter}, Loss: {print_loss_avg}')
+
     raise Exception("Implement the rest of me to train your encoder-decoder model")
